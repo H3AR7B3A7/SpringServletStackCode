94b621452fd8a3ca265752c7f5375fab742e940c
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-11-07 07:44:33 +0100

    :rocket: Initial commit

---
+++ src/main/java/be/dog/d/steven/DispatcherConfig.java
@@ -0,0 +1,26 @@
+package be.dog.d.steven;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("be.dog.d.steven")
+public class DispatcherConfig {
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix("/WEB-INF/");
+        vr.setSuffix(".jsp");
+
+        return vr;
+    }
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+}
